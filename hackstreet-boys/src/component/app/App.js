e52dcc0372c46ca9cc8f673c26a523e2fe0c9e30
Qas1m0m0 <Qasim.17@hotmail.com>
2023-04-25 15:02:04 +0100

    Working dropdown

--- hackstreet-boys/src/component/app/App.js
+++ hackstreet-boys/src/component/app/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Timeline from "../timeline";
+import CollapsibleTable from "../dropdown";
 
 function App() {
     return (
         <div className="App">
             <h1>SoC Roadmap App</h1>
             <Timeline />
+            <CollapsibleTable />
         </div>
     );
 }

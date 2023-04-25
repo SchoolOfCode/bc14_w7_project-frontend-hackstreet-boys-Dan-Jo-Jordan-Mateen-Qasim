c7e766e8956295276b8af2dfc5cbe586e5b2d66b
danben1 <124446045+danben1@users.noreply.github.com>
2023-04-25 14:16:10 +0100

    component folder skeleton created

---
+++ hackstreet-boys/src/component/app/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Timeline from "../timeline";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>SoC Roadmap App</h1>
+            <Timeline />
+        </div>
+    );
+}
+
+export default App;

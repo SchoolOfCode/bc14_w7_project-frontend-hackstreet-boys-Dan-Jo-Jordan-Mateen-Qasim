9ef079261fb7f9b3d543ce81859966ea4e930bea
MateenSQ <111274354+MateenSQ@users.noreply.github.com>
2023-04-26 16:09:16 +0100

    added SoC image, some basic styling in App.js and App.css

--- hackstreet-boys/src/component/app/App.js
+++ hackstreet-boys/src/component/app/App.js
@@ -8,6 +8,8 @@ function App() {
     return (
         <div className="App">
             <h1>SoC Roadmap App</h1>
+            <h2>'Your one-stop-shop for your weekly learning resources!'</h2>
+            <img src="https://www.schoolofcode.co.uk/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png" alt="School of Code Logo"></img>
             <Timeline />          
         </div>
     );

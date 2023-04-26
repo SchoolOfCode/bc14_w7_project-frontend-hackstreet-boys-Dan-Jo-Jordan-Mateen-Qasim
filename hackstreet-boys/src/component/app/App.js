bfa00d887413dd5144a2aab0ecf8234d74ee5c5e
danben1 <124446045+danben1@users.noreply.github.com>
2023-04-26 16:46:26 +0100

    Lord K added

--- hackstreet-boys/src/component/app/App.js
+++ hackstreet-boys/src/component/app/App.js
@@ -16,16 +16,18 @@ function App() {
         <div className="App">
             <h1>SoC Roadmap App</h1>
             <h2>'Your one-stop-shop for your weekly learning resources!'</h2>
+            <img
+                src="https://d33wubrfki0l68.cloudfront.net/e6fddcbea146f91d2f3c160f7d56a9391a4740b0/4e758/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png"
+                alt="SoC logo"
+            />
             {buttonClicked ? ( // conditionally render the Timeline component or the button
                 <Timeline />
             ) : (
                 <div>
-                    <div>
-                        <img
-                            src="https://d33wubrfki0l68.cloudfront.net/e6fddcbea146f91d2f3c160f7d56a9391a4740b0/4e758/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png"
-                            alt="SoC logo"
-                        />
-                    </div>
+                    <img
+                        src="https://i.dailymail.co.uk/i/pix/scaled/2013/08/03/article-2383977-0B66F6C300000578-81_310x187.jpg"
+                        alt="SoC logo"
+                    />
                     <button onClick={handleClick}>Enter</button>
                 </div>
             )}

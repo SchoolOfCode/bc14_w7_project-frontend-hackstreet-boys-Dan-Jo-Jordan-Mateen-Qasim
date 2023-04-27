2fb7abcdcb4d47be3ca25182fa55f3a9a6d45e6f
MateenSQ <111274354+MateenSQ@users.noreply.github.com>
2023-04-27 14:31:14 +0100

    small css changes

--- hackstreet-boys/src/component/app/App.js
+++ hackstreet-boys/src/component/app/App.js
@@ -10,13 +10,15 @@ function App() {
     function handleClick() {
         console.log("Hello");
         setButtonClicked(true); // update state variable
+        const appDiv = document.querySelector('.App');
+        appDiv.style.backgroundImage = "none";
+        appDiv.style.backgroundColor = `whitesmoke`;
     }
 
     return (
         <div className="App">
-        <p></p>
         {buttonClicked ? <p></p> : <h1>The Hackstreet Boys Present...</h1> }
-        <h2>The School of Code Roadmap App</h2>
+        {buttonClicked ? <h2 className = "black">The School of Code Roadmap App</h2> : <h2 className="white">The School of Code Roadmap App</h2> }
         <img src="https://d33wubrfki0l68.cloudfront.net/e6fddcbea146f91d2f3c160f7d56a9391a4740b0/4e758/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png"
             alt="SoC logo" />
             {buttonClicked ? ( // conditionally render the Timeline component or the button
